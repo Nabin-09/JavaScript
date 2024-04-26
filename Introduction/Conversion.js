1cc4d75f60856b95d4e20c6d99f460c40328a386
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-26 16:55:33 +0530

    Conversion of Data Types

---
+++ Introduction/Conversion.js
@@ -0,0 +1,14 @@
+/*
+let score = "34a"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+OUTPUT : 
+string
+number
+NaN  
+*/
+    let isLoggedIn = 1
+    let booleanLoggedIn = Boolean(isLoggedIn)
+    console.table([typeof(isLoggedIn),isLoggedIn, typeof booleanLoggedIn, booleanLoggedIn])
