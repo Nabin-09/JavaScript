f42e213fe1b7e945c38dc79c42fb4e1049514732
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-26 17:16:58 +0530

    Operations

---
+++ Introduction/Operations.js
@@ -0,0 +1,34 @@
+/*
+let value = 3;
+let negValue = -value
+console.log(value);
+console.log(negValue);
+console.table([value+value,value*value,value/4,value**value,value%2]);
+OUTPUT : 
+3
+-3
+┌─────────┬────────┐
+│ (index) │ Values │
+├─────────┼────────┤
+│ 0       │ 6      │
+│ 1       │ 9      │
+│ 2       │ 0.75   │
+│ 3       │ 27     │
+│ 4       │ 1      │
+└─────────┴────────┘
+*/
+let str1 = "Nabin "
+let str2 = "Sharma"
+console.log(str1 + str2); // Prints Nabin Sharma 
+console.log("1" + 2);
+console.log("1" + "2");
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+/*OUTPUT 
+12
+12
+12
+122
+32
+*/
