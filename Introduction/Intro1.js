424ab3d3008b2549542aea13a597edeb9ac8f5d9
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-26 15:38:38 +0530

    Variables and Constants

---
+++ Introduction/Intro1.js
@@ -0,0 +1,12 @@
+// Constants : Unchanged values once declared.
+const accountID = 6566445 // declaration
+
+// Variables : containers to store value.
+let accountMail = "nabinhaigol@gmail.com"
+var AccountPassword = "982902"
+accountCity = "Siliguri"//very bad way of declaring variable.
+//  accountID = 8998238 Throws error cant change const values.
+//LETS CHANGE VLAUES OF VARIABLES - 
+var AccountPassword = "982789"
+accountCity = "Noida"
+console.table([accountMail,accountID,accountCity,AccountPassword]);
