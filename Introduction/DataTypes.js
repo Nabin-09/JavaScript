e09e745cee0e10c47d3865c18a923f106588f5b3
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-26 16:39:02 +0530

    Data Types

---
+++ Introduction/DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as the newer version 
+let name = "Nabin"
+let age = 20;
+let isLoggesIn = false
+//DATA TYPES 
+// -(2^53 − 1) ( Number.MIN_SAFE_INTEGER ) to 2^53 − 1
+//use bigint for number of larger range
+/*Other data types :
+string = "kullu"
+boolean = true/false
+null = standalone value(khaali hai)
+undefined = When values aren't defined
+symbol =  unique
+Object 
+*/
