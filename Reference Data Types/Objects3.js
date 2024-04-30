696f1b2263268fff3c09e59dbb293cdcf9f64d42
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-30 12:53:32 +0530

    Objects - Advanced

---
+++ Reference Data Types/Objects3.js
@@ -0,0 +1,10 @@
+const user ={
+    name : "Nabin ",
+    age : 18,
+    UID : 76621812,
+    Username : {
+        firsttag : "nab",
+        secondtag : "8976"
+    }
+}
+console.log(user["name"]);
