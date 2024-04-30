1e282d2a31027425ed51333d3f97e612590b974e
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-04-30 12:48:07 +0530

    Notes updated

--- Reference Data Types/Objects2.js
+++ Reference Data Types/Objects2.js
@@ -23,5 +23,6 @@ const obj2 = {
     3 : "c",
     4 : "d"
 }
-const obj3  = {obj1,obj2} // creates object inside Object not preferred
+// const obj3  = {obj1,obj2} // creates object inside Object not preferred
+const obj3 = Object.assign(obj1,obj2)
 console.log(obj3);
