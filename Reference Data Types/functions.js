1aa5f29d465ec9396b05682dfd75f69be90be08c
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-01 12:44:11 +0530

    Started Functions

---
+++ Reference Data Types/functions.js
@@ -0,0 +1,14 @@
+
+function print(){
+    console.log("NABIN");
+}
+print()
+function AddNumber(num1,num2){
+    console.log(num1 + num2);
+}
+// AddNumber()//Prints NaN
+// AddNumber(3,"3")//33
+// AddNumber(3,"a")//3a
+// AddNumber("a",3)//a3
+// AddNumber(3,null)//3
+const result = AddNumber(3,5) 
