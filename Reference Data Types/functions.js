f1c6f401194190430a95483c821ab7ceb7b38e6a
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-01 12:53:02 +0530

    Functions Basics done

--- Reference Data Types/functions.js
+++ Reference Data Types/functions.js
@@ -12,3 +12,13 @@ function AddNumber(num1,num2){
 // AddNumber("a",3)//a3
 // AddNumber(3,null)//3
 const result = AddNumber(3,5) 
+function Addnums(num1,num2){
+   let result = num1+num2;
+   return result;
+}
+const res = Addnums(8,9)
+console.log(res);
+function LoginMessage(username){
+    return `${username} just Logged in` 
+}
+console.log(LoginMessage("Nabin"));
