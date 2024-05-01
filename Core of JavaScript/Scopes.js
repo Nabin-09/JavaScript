5cd8b5afb6837521ee338b3794daecf7ceccd4d4
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-01 13:23:32 +0530

    Scopes Introduction

---
+++ Core of JavaScript/Scopes.js
@@ -0,0 +1,11 @@
+// let a = 10;
+// const b = 21;
+// console.log(a);//10 
+// console.log(b);//21
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+console.log(a);//a is not defined 
+console.log(c);//30 hence use of var is deprecated.
