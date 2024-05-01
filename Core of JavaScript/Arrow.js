1c6315a11623fc9906c4227e5dc43366218b1d63
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-01 13:59:50 +0530

    This in Standalone VS Console

--- Core of JavaScript/Arrow.js
+++ Core of JavaScript/Arrow.js
@@ -18,3 +18,29 @@ user.WelcomeMessage();//Sam, welcome to website
   WelcomeMessage: [Function: WelcomeMessage]
 } 
 */
+console.log(this); // {}
+function chai(){
+    let username = "Nitesh"
+    console.log(this);
+}
+chai()
+/* OUTPUT : <ref *1> Object [global] {
+  global: [Circular *1],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  structuredClone: [Function: structuredClone],
+  atob: [Getter/Setter],
+  btoa: [Getter/Setter],
+  performance: [Getter/Setter],
+  fetch: [Function: fetch],
+  crypto: [Getter]
+} */ 
