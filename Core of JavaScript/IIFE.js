177d120e8b9e99bbb34f0996ab05cb49bf966b58
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-02 20:54:38 +0530

    IIFE-Done

--- Core of JavaScript/IIFE.js
+++ Core of JavaScript/IIFE.js
@@ -2,4 +2,12 @@
 function chai(){
     console.log(`DB connected`);
 }
-chai()
+chai(); //(use ; while using IIFE ,prints DB connected
+(function addTwo(){
+    console.log(`This is IIFE`)
+}) // first paranthesis si for definition of function 
+(); // parentheseis for execution prints this is IIFE
+/*Passing Parameters to Fucntion */
+((name) => {
+    console.log(`DB connected ${name}`);
+})('Nabin'); //Prints DB connected Nabin
