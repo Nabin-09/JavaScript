e3a2bebc44c7b89640f38a7c0a00fb8d2cfe4551
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-02 21:17:58 +0530

    JS-Execution

---
+++ Core of JavaScript/Java_Execution_Context.js
@@ -0,0 +1,10 @@
+let val1 = 10;
+let val2 = 5;
+function addNum(num1,num2){
+let total = num1 + num2
+return total
+}
+    let result = addNum(val1,val2)
+    let result2 = addNum(10,2)
+console.log((result));
+console.log(result2);
