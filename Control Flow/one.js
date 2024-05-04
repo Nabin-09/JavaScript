0779bd0e05a03731bcb8f8c55c27a242e255eecb
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-04 13:34:39 +0530

    Relational Operators

--- Control Flow/one.js
+++ Control Flow/one.js
@@ -11,3 +11,11 @@ if(userLoggedIn){
 }
 /* relational OPerators :
  <,>.!=,==,===,<=,>= */
+ /* If - Else combination , if does not execute the else block occurs,hence these are block statements */
+ const temp = 11;
+ if(temp > 35){
+    console.log(`Its Hot`);
+ }
+ else{
+    console.log(`Its chilled`);//this block runs
+ }
