7f3079b572d17f1b2d16336671ffb43f0e9f46d4
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-04 14:30:34 +0530

    For of

---
+++ Control Flow/ArrayIterators.js
@@ -0,0 +1,10 @@
+//for of 
+const arr = [1,2,3,4,5]
+for (const val of arr) //for(iterator of Object) -> Object refers to a thing and not the JS object
+    {
+    console.log(val);
+}//1 2 3 4 5
+const greeting = "HORNY HO "
+for(const greet of greeting){
+    console.log(`Each char is ${greet}`);
+}
