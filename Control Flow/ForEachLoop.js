8bd891f438676033f1eb9533cc61efc0395906f3
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-05-05 15:17:05 +0530

    high Order loops

--- Control Flow/ForEachLoop.js
+++ Control Flow/ForEachLoop.js
@@ -11,3 +11,26 @@ lang.forEach(function greet(val){
 lang.forEach((item) =>{
     console.log(item);//prints all items in array
 })
+/*Alternate way of using for each
+function printme (items) {
+    console.log(items);
+}
+lang.forEach(printme)
+*/
+myCoding  = [
+    {
+        langname : 'cpp',
+        langfile : 'cpp'
+    },
+    {
+        langname : 'java',
+        langfile : 'file'
+    },
+    {
+        langname : 'JAvascript',
+        langfile : 'JS'
+    }
+]
+myCoding.forEach((item) => {
+    console.log(item.langname);
+})
