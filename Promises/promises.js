183c9f1bf7e85109f24602bad3a5205a2c92cfb5
Nabin <sharmanabin.0910@gmail.com>
2025-05-29 14:45:17 +0530

    Using fetch and async await

--- Promises/promises.js
+++ Promises/promises.js
@@ -51,7 +51,7 @@ promiseFour.then(function(user){
 
 const promiseFive = new Promise(function(resolve , reject){
     setTimeout(function(){
-        let error = true;
+        let error = false;
         if(!error){
             resolve({user:"Nabin", pass:"xyz"})
         }else{
@@ -65,3 +65,59 @@ async function consumePromiseFive(){
     console.log(response)
 }
 consumePromiseFive()
+
+async function getAllUsers(){
+    try{
+    const response = await fetch('https://api.github.com/users/Nabin-09')
+    const data = await response.json();
+    console.log(data);
+    }catch(error){
+        console.log("E:",error);
+    }
+}
+getAllUsers()
+//output 
+/*
+Async task 1
+Async task 2
+404 Buddy
+{
+  login: 'Nabin-09',
+  id: 136674695,
+  node_id: 'U_kgDOCCV9hw',
+  avatar_url: 'https://avatars.githubusercontent.com/u/136674695?v=4',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/Nabin-09',
+  html_url: 'https://github.com/Nabin-09',
+  followers_url: 'https://api.github.com/users/Nabin-09/followers',
+  following_url: 'https://api.github.com/users/Nabin-09/following{/other_user}',
+  gists_url: 'https://api.github.com/users/Nabin-09/gists{/gist_id}',
+  starred_url: 'https://api.github.com/users/Nabin-09/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/Nabin-09/subscriptions',
+  organizations_url: 'https://api.github.com/users/Nabin-09/orgs',
+  repos_url: 'https://api.github.com/users/Nabin-09/repos',
+  events_url: 'https://api.github.com/users/Nabin-09/events{/privacy}',
+  received_events_url: 'https://api.github.com/users/Nabin-09/received_events',
+  type: 'User',
+  user_view_type: 'public',
+  site_admin: false,
+  name: 'Nabin Sharma',
+  company: null,
+  blog: 'https://leetcode.com/nabin_09/',
+  location: 'Greater Noida , Uttar Pradesh , Bharat.',
+  email: null,
+  hireable: null,
+  bio: 'Would love to be an Innovator rather than just another manager.\r\n',
+  twitter_username: null,
+  public_repos: 28,
+  public_gists: 0,
+  followers: 10,
+  following: 10,
+  created_at: '2023-06-15T05:02:29Z',
+  updated_at: '2025-05-26T12:08:36Z'
+}
+Promise hai ye 
+Node consumed
+{ username: 'Nabin', email: 'sharmanabin@gmail.com' }
+{ user: 'Nabin', pass: 'xyz' }
+*/
