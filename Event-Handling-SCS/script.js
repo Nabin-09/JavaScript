2c6ae736b80262c39e30b4e848aafeb84db786ae
Nabin <sharmanabin.0910@gmail.com>
2025-10-01 11:23:52 +0530

    Event listeners on Selection

---
+++ Event-Handling-SCS/script.js
@@ -0,0 +1,22 @@
+// let h1 = document.querySelector('h1');
+
+// h1.addEventListener('click' , ()=>{
+//     h1.style.color = 'red'
+// })
+
+// h1.removeEventListener('dblclick' the same function that was passed in eventListener)
+
+// let input = document.querySelector('input');
+
+// input.addEventListener('input' , function(det){
+//     if(det.data !== null){
+//         console.log(det.data);
+//     }
+// })
+
+// let header = document.querySelector('#h')
+// let sel = document.querySelector('select');
+
+// sel.addEventListener('change' , function(det){
+//     header.textContent = `${det.target.value} selected`
+// })
